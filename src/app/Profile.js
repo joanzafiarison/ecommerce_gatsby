6973cd6ae59cc05b4c2eedd48c54f6207e7536cf
joanzafdev <joanzafdev@gmail.com>
2023-08-24 05:34:04 +0200

    travail sur l'update des informations, a suivre , les validations

--- src/app/Profile.js
+++ src/app/Profile.js
@@ -6,47 +6,71 @@ const ProfilePage = () => {
     // récupération des infos
     const [userData, setUserData] = useState(getUser());
     const [currentDatas, setCurrentDatas] = useState([]);
-    //console.log("profile data",userData)
+    const  [openModification, setOpenModification] = useState(false);
+    const [valid, setValid] =useState(false);
+
+    console.log("profile data",userData)
     function handleSubmit(e){
         e.preventDefault();
     }
-    function modifyData(name) {
-        let newData = [...currentDatas];
-        //console.log(currentDatas.find(name));
-        console.log("before ",newData)
-        if(currentDatas.findIndex( d => d === name) !== -1){
-            newData = currentDatas.filter((v,k) => v !== name)
-            console.log("after filter",newData)
-            setCurrentDatas(newData)
+
+    function handleChange(e){
+        console.log(e.target.name);
+        console.log(e.target.value);
+        if (userData[e.target.name] !== e.target.value ){
+            setUserData({...userData,[e.target.name] : e.target.value})
         }
-        else {
-            newData.push(name);
-            setCurrentDatas(newData);
-            console.log("not found",newData);
-        }
-        
     }
-    /*
+
+    function handleValidation() {
+        console.log('validation of data');
+        //numéro
+        //email
+        //postal
+        //mot de passe
+    }
+
     useEffect(()=>{
-        setUserData()
-    },[])*/
+        //envoie des nouvelles données? 
+        // en attente, attendre c=nf=f nfionfirmation via email
+        console.log(userData)
+    },[valid])
     return(
         <div className='base_container'>
             <h1>Information sur l'utilisateur </h1>
             <form onSubmit={handleSubmit}>
                 <label> 
                     Identifiants :
-                    <input type="text" name="username" value={userData.username} disabled={ currentDatas.indexOf("username") !== -1 ?  "" : "disabled"}/>
-                    <button onClick={() => modifyData("username")}>Changer</button>
+                    <input type="text" name="username" value={userData.username} disabled={ openModification ?  "" : "disabled"} onChange={(e) => handleChange(e)}/>
                 </label>
                 <label>  
                     Email :
-                    <input type="text" name="email" value={userData.email} disabled={currentDatas.indexOf("email") !== -1 ?  "" : "disabled"}/>
-                    <button onClick={()=> modifyData("email")}>Changer</button>
+                    <input type="text" name="email" value={userData.email} disabled={ openModification ?  "" : "disabled"} onChange={(e) => handleChange(e)}/>
                 </label>
+                <label>  
+                    Mot de passe :
+                    <input type="password" name="password" value={userData.password} disabled={ openModification ?  "" : "disabled"} onChange={(e) => handleChange(e)}/>
+                </label>
+                <label>  
+                    Adresse :
+                    <input type="text" name="adress" value={userData.adress} disabled={ openModification ?  "" : "disabled"} onChange={(e) => handleChange(e)}/>
+                </label>
+                <label>  
+                    Postal:
+                    <input type="text" name="postal" value={userData.postal} disabled={ openModification ?  "" : "disabled"} onChange={(e) => handleChange(e)}/>
+                </label>
+                <label>  
+                    Pays :
+                    <input type="text" name="country" value={userData.country} disabled={ openModification ?  "" : "disabled"} onChange={(e) => handleChange(e)}/>
+                </label>
+                <label>  
+                    Téléphone :
+                    <input type="text" name="phone" value={userData.phone} disabled={ openModification ?  "" : "disabled"} onChange={(e) => handleChange(e)}/>
+                </label>
+                <button disabled={ openModification ? "" : "disabled"} style={{width:150}} onClick={() => handleValidation()} >Valider</button>
             </form> 
       
-            <button style={{width:150}}>Changer le mot de passe</button>
+            <button style={{width:150}} onClick={()=> setOpenModification(!openModification)}>Modifier les informations</button>
 
         </div>
     )
